9d8fb05609a12f60ded816db11c89436a0df8705
jrufasto5 <jrufasto5@2fe365f3-be8f-1562-c7ce-7a01983a3a99>
2010-06-21 07:00:46 +0000

    AGREGANDO SERVICIO USUARIO

---
+++ i-gastronomico/src/main/java/pe/edu/upc/dew/igastronomico/service/UsuarioService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.igastronomico.service;
+import java.util.ArrayList;
+import java.util.List;
+import pe.edu.upc.dew.igastronomico.actionform.*;
+
+/**
+ *
+ * @author Junior
+ */
+public class UsuarioService {
+
+}
