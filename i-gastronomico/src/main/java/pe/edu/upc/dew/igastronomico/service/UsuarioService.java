9ef994d0a4e5621f24e9b861f89ab06c2084770a
jrufasto5 <jrufasto5@2fe365f3-be8f-1562-c7ce-7a01983a3a99>
2010-06-21 19:51:20 +0000

    actualizando service usuario

--- i-gastronomico/src/main/java/pe/edu/upc/dew/igastronomico/service/UsuarioService.java
+++ i-gastronomico/src/main/java/pe/edu/upc/dew/igastronomico/service/UsuarioService.java
@@ -7,11 +7,36 @@ package pe.edu.upc.dew.igastronomico.service;
 import java.util.ArrayList;
 import java.util.List;
 import pe.edu.upc.dew.igastronomico.actionform.*;
+import pe.edu.upc.dew.igastronomico.bean.UsuarioBean;
 
 /**
  *
  * @author Junior
  */
 public class UsuarioService {
+    
+    private List<UsuarioBean> listaUsuarios;
+
+     UsuarioBean usu1 = new UsuarioBean("U0001","Junior Rufasto","123",1,"20/06/2010","jrufasto5@gmail.com");
+            UsuarioBean usu2 = new UsuarioBean("U0002","Jose Huaman","123",2,"20/06/2010","jhuaman@gmail.com");
+            UsuarioBean usu3 = new UsuarioBean("U0003","Carlos Martinez","123",1,"20/06/2010","cmartinez@gmail.com");
+
+
+            List<UsuarioBean> listUsuarios = new ArrayList<UsuarioBean>();
+            listUsuarios.add(usu1);
+            listUsuarios.add(usu2);
+            listUsuarios.add(usu3);
+         
+         
+
+
+     /**
+      * @return the listaAgencias
+      */
+     public List<Agencia> getListaAgencias() {
+         return listaAgencias;
+     }
+
+
 
 }
