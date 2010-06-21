b84db6c05243f86bcab6d5992b0d5219d7e22f09
jrufasto5 <jrufasto5@2fe365f3-be8f-1562-c7ce-7a01983a3a99>
2010-06-21 07:02:51 +0000

    AGREGANDO ACTIONFORM

---
+++ i-gastronomico/src/main/java/pe/edu/upc/dew/igastronomico/actionform/LoginForm.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.igastronomico.actionform;
+
+/**
+ *
+ * @author Junior
+ */
+//HDHDHDHDDHDH
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+public class LoginForm extends ActionForm {
+
+private String usuario;
+private String password;
+
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getUsuario() {
+	return usuario;
+}
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+@Override
+public void reset(ActionMapping mapping, HttpServletRequest request) {
+	// TODO Auto-generated method stub
+	super.reset(mapping, request);
+}
+@Override
+public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+	// TODO Auto-generated method stub
+	return super.validate(mapping, request);
+}
+
+}
